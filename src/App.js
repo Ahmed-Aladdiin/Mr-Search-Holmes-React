1491c20e998a4b848a16364b78dea4dac276faef
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-12 01:42:40 +0200

    apply light theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import Home from './screens/Home/Home'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import ThemeContext from "ThemeContext";
+import Home from "./screens/Home/Home";
 
 function App() {
+  const [isDark, setIsDark] = useState(1);
+  const setDark = () => {
+    setIsDark(1);
+    document.querySelector('body').setAttribute('data-theme', 'dark');
+  }
+  const setLight = () => {
+    setIsDark(0);
+    document.querySelector('body').setAttribute('data-theme', 'light');
+  }
+
   return (
-    <Home/>
+    <ThemeContext.Provider value={{ isDark, setDark, setLight }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
