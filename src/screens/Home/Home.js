4ed044136f4d6fae9c8426a2a5c83e26e97695a8
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-12 01:41:34 +0200

    Home page component

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,15 +1,35 @@
-import logo from './../../imgs/sherlock-white.svg';
-import './Home.css';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import ThemeContext from "ThemeContext";
+
+import Moon from "imgs/moon.png";
+import Sun from "imgs/sun.png";
+import Logo from "./Logo";
+import "./Home.css";
+import Input from "./Input";
 
 function App() {
+  const { isDark, setDark, setLight } = useContext(ThemeContext);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Mr Searchlock Holmes
+        <div></div>
+        <div>
+          <Logo isDark={isDark}/>
+          <Input />
+        </div>
+        <p id="Input-Post-Text">
+          Send me your query, and surely the truth will be unveiled, or you{" "}
+          <br />
+          may pay me a visit at{" "}
+          <Link to="https://maps.app.goo.gl/ceR9pVoZEzKFwtLD7" className="Link">
+            221B Baker Street
+          </Link>
         </p>
-        <input type='text' placeholder='Enter your Query'/>
+        <div id="Theme-Switch" onClick={isDark ? setLight : setDark}>
+          <img src={isDark ? Sun : Moon} />
+        </div>
       </header>
     </div>
   );
