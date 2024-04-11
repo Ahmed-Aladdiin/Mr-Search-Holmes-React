5c269315b74002e24313dfa7493aa57ddfba6e0f
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-12 01:40:07 +0200

    input component

---
+++ src/screens/Home/Input.js
@@ -0,0 +1,15 @@
+import send from 'imgs/send.svg'
+import './Input.css'
+
+function Input() {
+  return (
+    <div id="Input-Field-Container">
+      <input type="text" placeholder="Enter your Query" id="Query-Input" />
+      <div id="Send-Button-Container">
+        <img src={send} id="Send-Button"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
