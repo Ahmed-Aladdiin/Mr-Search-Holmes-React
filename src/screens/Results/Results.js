1ac3c71c26e280f9ad0841323197bdc0cce03d4a
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-12 16:57:25 +0200

    Add responsive styles and fetch error handling

--- src/screens/Results/Results.js
+++ src/screens/Results/Results.js
@@ -1,42 +1,52 @@
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
+// import { useLocation } from 'react-router-dom';
 
 import Input from "components/Input";
 import Logo from "components/Logo";
 import ThemeButton from "components/ThemeButton";
+import ResultsList from "./sub/ResultsList";
 
-import "./Results.css"
+import "./Results.css";
 
 function Results() {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const query = queryParams.get("query");
-  const [results, setResults] = useState(null);
+  // const location = useLocation();
+  // const queryParams = new URLSearchParams(location.search);
+  // const query = queryParams.get("query");
+  const [results, setResults] = useState([]);
   const baseURL = process.env.REACT_APP_BASE_URL;
-
   useEffect(() => {
     fetch(`${baseURL}/results`, {
-      method: "GET", 
-      headers: {
-        "Content-Type": "application/json"
-      },
+      method: "GET",
+      // headers: {
+      //   "Content-Type": "application/json"
+      // },
       // body: JSON.stringify({query: query})
     })
-    .then(response => response.json())
-    .then(data => setResults(data));
+      .then((response) => {
+        if (!response.ok) {
+          setResults([]);
+          throw new Error('Network response was not ok');
+        };
+        return response.json();
+      })
+      .then((data) => setResults(data))
+      .catch((e) => {
+        console.error("Can't reach server");
+      });
   }, []);
 
   return (
     <>
-      <ThemeButton/>
-      <div id="Logo-and-Input">
-        <Logo showText={false} size={10}/>
-        <Input/>
+      <ThemeButton />
+      <div className="Gap-Container">
+        <div className="Gap" />
+        <div id="Logo-and-Input">
+          <Logo showText={false} size={10} />
+          <Input />
+        </div>
       </div>
-      <div id="Horizontal-Line"/>
-      <div id="results">
-
-      </div>
+      <div id="Horizontal-Line" />
+      <ResultsList results={results} />
     </>
   );
 }
