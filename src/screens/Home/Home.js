befd5edd7d2b442a28b7b8b44db2fc4a9fe5c9e6
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-12 05:29:16 +0200

    same

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,35 +1,28 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import ThemeContext from "ThemeContext";
 
-import Moon from "imgs/moon.png";
-import Sun from "imgs/sun.png";
-
-import Logo from "./sub/Logo";
+import Logo from "../../components/Logo";
 import Input from "./sub/Input";
 
 import "./Home.css";
+import ThemeButton from "components/ThemeButton";
 
 function App() {
-  const { isDark, setDark, setLight } = useContext(ThemeContext);
-
+  const SherlockLocation = "https://maps.app.goo.gl/ceR9pVoZEzKFwtLD7";
   return (
     <div className="App">
       <header className="App-header">
         <div></div>
         <div>
-          <Logo isDark={isDark}/>
+          <Logo/>
           <Input />
         </div>
         <p id="Input-Post-Text">
           Send me your query, and surely the truth will be unveiled, or you may pay me a visit at{" "}
-          <Link to="https://maps.app.goo.gl/ceR9pVoZEzKFwtLD7" className="Link">
+          <Link to={SherlockLocation} className="Link">
             221B Baker Street
           </Link>
         </p>
-        <div id="Theme-Switch" onClick={isDark ? setLight : setDark}>
-          <img src={isDark ? Sun : Moon} alt="theme switching icon"/>
-        </div>
+        <ThemeButton/>
       </header>
     </div>
   );
