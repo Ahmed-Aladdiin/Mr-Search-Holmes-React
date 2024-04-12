e8d63dd3a8f58b35f5a0c4621061061f2ba5eaae
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-12 16:56:24 +0200

    Add ListItem.css and update ResultsList.js

--- src/screens/Results/sub/ResultsList.js
+++ src/screens/Results/sub/ResultsList.js
@@ -1,9 +1,26 @@
-
-function ResultsList ({results=[]}) {
-
+import { Link } from "react-router-dom";
+import "./ListItem.css";
+function ResultsList({ results = [] }) {
   return (
-    <div id="Results-Container">
-      
+    <div className="Gap-Container">
+      <div className="Gap"/>
+      <div className="Gap"/>
+      <div className="Gap"/>
+      <div id="Results-Container">
+        {results.map((result) => (
+          <Result result={result} key={result.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Result({ result }) {
+  return (
+    <div className="ListItem">
+      <Link to={result.url} className="Result-Link"><h2>{result.title}</h2></Link>
+      <p className="url">{result.url}</p>
+      <p className="paragraph">{result.paragraph}</p>
     </div>
   );
 }
